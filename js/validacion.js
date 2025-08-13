60a6ef9fc9bdba8fd291541c0a34344ea6f4a3fa
leeachevalier <leandrochevalier21@gmail.com>
2025-08-13 14:50:15 -0300

    fix

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,28 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function closeAlert(){
+    document.getElementById("alert-success").classList.remove("show");
+    document.getElementById("alert-danger").classList.remove("show");
+}
+
+
+function sendForm(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log(data)
+    
+    if(data.password1 === data.password2 && data.terminos) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+}
+
+
+
+
